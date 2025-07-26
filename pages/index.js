d272df1932bff624ffbe86566546898c29a32403
Islamzhet <zhetislam@gmail.com>
2025-07-26 11:07:50 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 export default function Home() {
+  const [question, setQuestion] = useState('');
   const [answer, setAnswer] = useState('');
   const [result, setResult] = useState(null);
 
@@ -9,7 +10,7 @@ export default function Home() {
     const res = await fetch('/api/evaluate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ answer }),
+      body: JSON.stringify({ question, answer }),
     });
     const data = await res.json();
     setResult(data);
@@ -20,6 +21,16 @@ export default function Home() {
       <h1>Upgrade Your Knowledge</h1>
       <form onSubmit={handleSubmit}>
         <label>
+          Вопрос:
+          <input
+            type="text"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            style={{ marginLeft: 10, width: '300px' }}
+          />
+        </label>
+        <br /><br />
+        <label>
           Ответ на вопрос:
           <input
             type="text"
@@ -28,7 +39,8 @@ export default function Home() {
             style={{ marginLeft: 10, width: '300px' }}
           />
         </label>
-        <button type="submit" style={{ marginLeft: 10 }}>Отправить</button>
+        <br /><br />
+        <button type="submit">Оценить</button>
       </form>
       {result && (
         <div style={{ marginTop: 20 }}>
